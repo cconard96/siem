bcbed78b8309f1ac81d8ab8bbb1bbf8cc538ba2e
cconard96 <cconard96@gmail.com>
2020-10-11 14:11:16 -0400

    Add composer check for install

--- setup.php
+++ setup.php
@@ -89,6 +89,10 @@ function plugin_siem_check_prerequisites()
          return false;
       }
    }
+   if (!is_readable(__DIR__ . '/vendor/autoload.php') || !is_file(__DIR__ . '/vendor/autoload.php')) {
+      echo "Run composer install --no-dev in the plugin directory<br>";
+      return false;
+   }
    return true;
 }
 
