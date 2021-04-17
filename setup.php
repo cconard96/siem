80fa47917aef06b1887e0680bf8a3d1ed86cc242
Curtis Conard <cconard96@gmail.com>
2021-04-17 12:43:12 -0400

    Bump GLPI version support

--- setup.php
+++ setup.php
@@ -22,7 +22,7 @@
 
 define('PLUGIN_SIEM_VERSION', '1.0.0');
 define('PLUGIN_SIEM_MIN_GLPI', '9.5.0');
-define('PLUGIN_SIEM_MAX_GLPI', '9.6.0');
+define('PLUGIN_SIEM_MAX_GLPI', '11.0.0');
 
 function plugin_init_siem()
 {
